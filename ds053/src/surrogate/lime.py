c6cfacc9511c7881c29f2668fa018332b3fd5ed1
youjin2 <ygene.lee@gmail.com>
2025-01-10 22:14:45 +0900

    ENH: lime image util

--- ds053/src/surrogate/lime.py
+++ ds053/src/surrogate/lime.py
@@ -1,7 +1,12 @@
-from typing import Callable, List
+from typing import Callable, List, Tuple
 
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.color import label2rgb
 from lime.lime_text import LimeTextExplainer
+from lime.lime_image import LimeImageExplainer
 from lime.explanation import Explanation
+from lime.wrappers.scikit_image import SegmentationAlgorithm
 
 
 class LimeTextSummarizer:
@@ -63,3 +68,78 @@ class LimeTextSummarizer:
         explanation.show_in_notebook(text=input_text, labels=available_labels[:num_display_labels])
 
         return explanation
+
+
+class LimeImageSummarizer:
+    def __init__(
+        self,
+        explainer: LimeImageExplainer,
+        segmentation_fn: SegmentationAlgorithm,
+        classifier_fn: Callable,
+    ) -> None:
+        self.explainer = explainer
+        self.segmentation_fn = segmentation_fn
+        self.classifier_fn = classifier_fn
+
+    def set_data(
+        self,
+        X: np.ndarray,
+        y: np.ndarray
+    ) -> None:
+        assert len(X) == len(y), "length of X and y must be equivalent"
+        assert len(X.shape) == 4, "dim(X) must be 4-dimensional (batch_size, height, width, channel)"
+
+        self._X = X
+        self._y = y
+        self._n = len(X)
+
+    def __call__(
+        self,
+        idx: int,
+        top_labels: int = 6,
+        num_samples: int = 1000,
+        num_features: int = 8,
+        **kwargs,
+    ) -> Tuple[Explanation, plt.Figure]:
+        if idx < 0 or idx >= self._n:
+            raise IndexError(f"idx must be between [0, {self._n-1}")
+
+        explanation = self.explainer.explain_instance(
+            self._X[idx],
+            classifier_fn=self.classifier_fn,
+            top_labels=top_labels,
+            num_samples=num_samples,
+            segmentation_fn=self.segmentation_fn
+        )
+
+        tmp_pos, mask_pos = explanation.get_image_and_mask(
+            self._y[idx],
+            positive_only=True,
+            num_features=num_features,
+            hide_rest=kwargs.get("hide_rest", False)
+        )
+
+        tmp_all, mask_all = explanation.get_image_and_mask(
+            self._y[idx],
+            positive_only=False,
+            num_features=num_features,
+            hide_rest=kwargs.get("hide_rest", False)
+        )
+
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+        ax1.imshow(label2rgb(mask_pos, tmp_pos, bg_label=0))
+        ax1.set_title(f"Positive Regions for {self._y[idx]}")
+
+        ax2.imshow(label2rgb(4-mask_all, tmp_all, bg_label=0))
+        ax2.set_title(f"Positive/Negative Regions for {self._y[idx]}")
+
+        ax3.imshow(tmp_all)
+        ax3.set_title("Output Image Only")
+
+        ax4.imshow(mask_all)
+        ax4.set_title("Mask Only")
+
+        for ax in [ax1, ax2, ax3, ax4]:
+            ax.axis("off")
+
+        return (explanation, fig)
